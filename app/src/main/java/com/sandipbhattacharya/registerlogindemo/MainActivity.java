102445b34a52b9fe391faa48b5b59c837ff0f181
krishankant670 <krishankant.20eic523@rtu.ac.in>
2023-09-23 17:31:07 +0530

    login-page and register-page

---
+++ app/src/main/java/com/sandipbhattacharya/registerlogindemo/MainActivity.java
@@ -0,0 +1,71 @@
+package com.sandipbhattacharya.registerlogindemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
+    private String email, password;
+    private final String URL = "http://192.168.29.43:80/login/login.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email = password = "";
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+
+    public void login(View view) {
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        if(!email.equals("") && !password.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, response -> {
+                Log.d("res", response);
+                if (response.equals("success")) {
+                    Intent intent = new Intent(MainActivity.this, Success.class);
+                    startActivity(intent);
+                    finish();
+                } else if (response.equals("failure")) {
+                    Toast.makeText(MainActivity.this, "Invalid Login Id/Password", Toast.LENGTH_SHORT).show();
+                }
+            }, error -> Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show()){
+                @NonNull
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("email", email);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }else{
+            Toast.makeText(this, "Fields can not be empty!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+        finish();
+    }
+}
